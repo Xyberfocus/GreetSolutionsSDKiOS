c8268d0346ea80641fa6af18d275a01fd5c4102b
Yoder Macas <yoder.macasg@hotmail.com>
2020-05-21 15:34:32 -0500

    [YM] Arreglo de clase SendToRouter

--- GreetSolutions.podspec
+++ GreetSolutions.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
     s.name             = 'GreetSolutions'
-    s.version          = '1.1.0'
+    s.version          = '1.1.1'
     s.summary          = 'Library to recongnize people'
     
     # This description is used to generate tags and improve search results.
--- GreetSolutions/Classes/SendToRouter.swift
+++ GreetSolutions/Classes/SendToRouter.swift
@@ -9,45 +9,47 @@ import Foundation
 import PromiseKit
 
 public class SendToRouter {
-     public init() {}
-
+    public init() {}
     
-   public let httpRequestUser = httpRequestHelper()
+    
+    public let httpRequestUser = httpRequestHelper()
     public static func SendUniqueIdToLocalNetwork() -> Promise<NSDictionary>  {
-      return Promise<NSDictionary> { seal in
-        let UUIDUser : String =  (UserDefaults.standard.string(forKey: "gsId"))!
-        let urlString : String = "/post_uuid.php"
-        let stringPost : String = String(localServerIp) + urlString
-        let requestHeader : [String:String] = [ "Content-Type" : "application/x-www-form-urlencoded"]
+        return Promise<NSDictionary> { seal in
+            let UUIDUser : String =  (UserDefaults.standard.string(forKey: "gsId"))!
+            let localServerIp = "http://10.10.10.1"
+            var jsonResult : NSDictionary = [:]
+            let urlString : String = "/post_uuid.php"
+            let stringPost : String = String(localServerIp) + urlString
+            let requestHeader : [String:String] = [ "Content-Type" : "application/x-www-form-urlencoded"]
             var requestBodyComponents = URLComponents()
-         requestBodyComponents.queryItems = [URLQueryItem(name: "uuid", value: UUIDUser)]
-        var request = URLRequest(url: URL(string: stringPost)!)
+            requestBodyComponents.queryItems = [URLQueryItem(name: "uuid", value: UUIDUser)]
+            var request = URLRequest(url: URL(string: stringPost)!)
             request.httpMethod = "POST"
             request.allHTTPHeaderFields = requestHeader
             request.httpBody = requestBodyComponents.query?.data(using: .utf8)
-        _ = URLSession.shared.dataTask(with: request){(datos,response,error) in
-              if error != nil{
-                print("Error POST")
-                let errorPost = NSError(domain: " POST Failed Network", code: 100, userInfo: [NSLocalizedDescriptionKey: " POST Failed Network" ])
-                seal.reject(errorPost)
-              }else{
-                if let content = datos {
-                  do{
-                    let jsonResult = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-                    seal.fulfill(jsonResult)
-                  }catch{
-                    let errorPostRequest = NSError(domain: " POST Failed Request", code: 101, userInfo: [NSLocalizedDescriptionKey: " POST Failed Request" ])
-                    seal.reject(errorPostRequest)
-                  }
+            _ = URLSession.shared.dataTask(with: request){(datos,response,error) in
+                if error != nil{
+                    print("Error POST")
+                    let errorPost = NSError(domain: " POST Failed Network", code: 100, userInfo: [NSLocalizedDescriptionKey: " POST Failed Network" ])
+                    seal.reject(errorPost)
+                }else{
+                    if let content = datos {
+                        do{
+                            let jsonResult = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                            seal.fulfill(jsonResult)
+                        }catch{
+                            let errorPostRequest = NSError(domain: " POST Failed Request", code: 101, userInfo: [NSLocalizedDescriptionKey: " POST Failed Request" ])
+                            seal.reject(errorPostRequest)
+                        }
+                    }
                 }
-              }
             }.resume()
-      }
+        }
     }
 }
 
 
 
 
- 
- 
+
+
