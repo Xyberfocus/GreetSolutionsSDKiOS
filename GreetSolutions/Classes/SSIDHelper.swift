6bf66ae0946bd8aed53e85fb236eac311ffa40c6
Yoder Macas <yoder.macasg@hotmail.com>
2020-05-21 01:24:49 -0500

    [YM]Couding bugs SSID class

--- GreetSolutions/Classes/SSIDHelper.swift
+++ GreetSolutions/Classes/SSIDHelper.swift
@@ -14,7 +14,14 @@ import CoreLocation
 
 public class SSIDHelper{
   
-   public func startWifi() {
+    private init() {}
+//    public static func aylin() -> String {
+//        return "Aylin te amo"
+//    }
+
+    
+    
+    public static func startWifi() -> Bool {
     let defaults = UserDefaults.standard
     let userNumberSerial : String =  (UIDevice.current.identifierForVendor?.uuidString)!
     defaults.set(userNumberSerial, forKey: "gsId")
@@ -24,33 +31,46 @@ public class SSIDHelper{
          session = 1
          defaults.set(session, forKey: "sessionNumber")
          defaults.set(false, forKey: "UserRecognized")
-         self.connectSSIDWifi(ssid: "meshlium0534", passphrase: "xyber2020focus")
+        
+        let hotspotConfig = NEHotspotConfiguration(ssid: "meshlium0534", passphrase: "xyber2020focus", isWEP: false)
+        
+        NEHotspotConfigurationManager.shared.apply(hotspotConfig){ (error) in
+          if let error = error {
+            print("error = ",error)
+          }
+          else {
+            print("Success WIFI!")
+            
+          }
+        }
          defaults.set(false, forKey: "UserRegsiter")
-         
+        return true
+
        }else{
          let sessionNow = defaults.integer(forKey: "sessionNumber")
          session = sessionNow + 1
          print("Session number")
          print(session)
          defaults.set(session, forKey: "sessionNumber")
+        return false
        }
   }
   
-   public func connectSSIDWifi(ssid: String, passphrase: String)  {
-    
-    let hotspotConfig = NEHotspotConfiguration(ssid: ssid, passphrase: passphrase, isWEP: false)
-    
-    NEHotspotConfigurationManager.shared.apply(hotspotConfig){ (error) in
-      if let error = error {
-        print("error = ",error)
-      }
-      else {
-        print("Success WIFI!")
-        
-      }
-    }
-    
-  }
+//   public func connectSSIDWifi(ssid: String, passphrase: String)  {
+//
+//    let hotspotConfig = NEHotspotConfiguration(ssid: ssid, passphrase: passphrase, isWEP: false)
+//    
+//    NEHotspotConfigurationManager.shared.apply(hotspotConfig){ (error) in
+//      if let error = error {
+//        print("error = ",error)
+//      }
+//      else {
+//        print("Success WIFI!")
+//
+//      }
+//    }
+//
+//  }
   
   
 
