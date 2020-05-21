794357a200a9b92b927e95e59178dd2feb1abaec
Yoder Macas <yoder.macasg@hotmail.com>
2020-05-21 00:14:51 -0500

    [YM] Correción bug

--- GreetSolutions/Classes/SSIDHelper.swift
+++ GreetSolutions/Classes/SSIDHelper.swift
@@ -14,7 +14,7 @@ import CoreLocation
 
 public class SSIDHelper{
   
-   func startWifi() {
+   public func startWifi() {
     let defaults = UserDefaults.standard
     let userNumberSerial : String =  (UIDevice.current.identifierForVendor?.uuidString)!
     defaults.set(userNumberSerial, forKey: "gsId")
@@ -36,7 +36,7 @@ public class SSIDHelper{
        }
   }
   
-   func connectSSIDWifi(ssid: String, passphrase: String)  {
+   public func connectSSIDWifi(ssid: String, passphrase: String)  {
     
     let hotspotConfig = NEHotspotConfiguration(ssid: ssid, passphrase: passphrase, isWEP: false)
     
